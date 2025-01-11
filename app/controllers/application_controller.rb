a53610917900e71d1c1d453d5af151a49c75fdab
rendy2808 <rendy2808@gmail.com>
2025-01-11 22:43:41 +0700

    feat: create a validation method for api-key header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :validate_api_key
+
+  private
+
+  def validate_api_key
+    api_key = request.headers['X-API-KEY']
+    valid_key = 'key-12345' # Or ENV['API_KEY'], for technical test purpose
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless api_key == valid_key
+  end
 end
