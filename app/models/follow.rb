8959201fda5793032bfad2e006833ba5097e41bc
rendy2808 <rendy2808@gmail.com>
2025-01-12 11:01:37 +0700

    feat: add associations in Follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,9 @@
 class Follow < ApplicationRecord
+  # Associations
+  belongs_to :user, class_name: 'User'
+  belongs_to :followed_user, class_name: 'User'
+
+  # Validations
+  validates :user_id, presence: true
+  validates :followed_user_id, presence: true
 end
