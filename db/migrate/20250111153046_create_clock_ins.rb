09f657888e5daafadc89c7c46b089866766d43d8
rendy2808 <rendy2808@gmail.com>
2025-01-13 21:16:01 +0700

    feat: change column name in table clock_ins from type to clock_in_type

--- db/migrate/20250111153046_create_clock_ins.rb
+++ db/migrate/20250111153046_create_clock_ins.rb
@@ -2,7 +2,7 @@ class CreateClockIns < ActiveRecord::Migration[7.1]
   def change
     create_table :clock_ins do |t|
       t.integer :user_id
-      t.string :type
+      t.integer :clock_in_type
 
       t.timestamps
     end
