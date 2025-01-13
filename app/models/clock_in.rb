8a7e96631f398932c0c532ac1dc357793f6fe2c2
rendy2808 <rendy2808@gmail.com>
2025-01-13 21:16:59 +0700

    feat: add validation in clock_in model and type enum

--- app/models/clock_in.rb
+++ app/models/clock_in.rb
@@ -1,2 +1,10 @@
 class ClockIn < ApplicationRecord
+  # Associations
+  belongs_to :user, class_name: 'User'
+
+  # Validations
+  validates :user_id, presence: true
+  validates :clock_in_type, presence: true
+
+  enum clock_in_type: { good_night: 0, wake_up: 1 }
 end
